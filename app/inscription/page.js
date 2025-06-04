42cacb3b4ced7c21c84419fb4b914e5ff12285e6
Melanie <melanie.bruzac@outlook.fr>
2025-06-04 15:16:01 +0200

    remove inscription

--- app/inscription/page.js
+++ app/inscription/page.js
@@ -1,33 +1,33 @@
-'use client';
-import { useState } from 'react';
-import { signIn } from 'next-auth/react';
+// 'use client';
+// import { useState } from 'react';
+// import { signIn } from 'next-auth/react';
 
-export default function SignupPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+// export default function SignupPage() {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
 
-  async function handleSubmit(e) {
-    e.preventDefault();
+//   async function handleSubmit(e) {
+//     e.preventDefault();
     
-  }
+//   }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input 
-        type="email" 
-        placeholder="Email" 
-        value={email} 
-        onChange={(e) => setEmail(e.target.value)} 
-        required 
-      />
-      <input 
-        type="password" 
-        placeholder="Mot de passe" 
-        value={password} 
-        onChange={(e) => setPassword(e.target.value)} 
-        required 
-      />
-      <button type="submit">S'inscrire</button>
-    </form>
-  );
-}
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <input 
+//         type="email" 
+//         placeholder="Email" 
+//         value={email} 
+//         onChange={(e) => setEmail(e.target.value)} 
+//         required 
+//       />
+//       <input 
+//         type="password" 
+//         placeholder="Mot de passe" 
+//         value={password} 
+//         onChange={(e) => setPassword(e.target.value)} 
+//         required 
+//       />
+//       <button type="submit">S'inscrire</button>
+//     </form>
+//   );
+// }
