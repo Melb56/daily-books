65515a2582a3125e36f64ccdea0952ee6a43ba39
Melanie <melanie.bruzac@outlook.fr>
2025-06-05 14:59:32 +0200

    t10

--- app/admin/edit/[id]/page.js
+++ app/admin/edit/[id]/page.js
@@ -1,24 +1,23 @@
 import { prisma } from '@/lib/prisma'; 
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/lib/auth';
-// import { auth } from '@/lib/auth';
+// import { getServerSession } from 'next-auth';
+// import { authOptions } from '@/lib/auth';
+import { auth } from '@/lib/auth';
 import { redirect } from 'next/navigation';
 import EditForm from '@/components/EditForm';
 
 
 
 export default async function EditArticlePage({ params }) {
-  // const articleId = parseInt(params.id, 10);
-   const numericId = parseInt(params.id, 10);
-  const session = await getServerSession(authOptions);
-  // const resolvedParams = await params; 
-  // const { id } = resolvedParams; 
-  // const session = await auth()
+  //const articleId = parseInt(params.id, 10);
+  //const session = await getServerSession(authOptions);
+  const resolvedParams = await params; 
+  const { id } = resolvedParams; 
+  const session = await auth();
 
   if (!session || session.user.role !== 'ADMIN') {
     redirect('/login');
   }
-  //  const numericId = parseInt(id, 10);
+  const numericId = parseInt(id, 10);
 
   const article = await prisma.article.findUnique({
     where: { id: numericId },
--- app/admin/new/page.js
+++ app/admin/new/page.js
@@ -1,19 +1,19 @@
-// import { getServerSession } from 'next-auth';
-// import { authOptions } from '@/lib/auth';
-// import { redirect } from 'next/navigation';
-// import ArticleForm from '@/components/ArticleForm';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
+import { redirect } from 'next/navigation';
+import ArticleForm from '@/components/ArticleForm';
 
-// export default async function NewArticlePage() {
-//   const session = await getServerSession(authOptions);
+export default async function NewArticlePage() {
+  const session = await getServerSession(authOptions);
 
-//   if (!session || session.user.role !== 'ADMIN') {
-//     redirect('/login');
-//   }
+  if (!session || session.user.role !== 'ADMIN') {
+    redirect('/login');
+  }
 
-//   return (
-//     <div className="admin-new-article">
-//       <h1>Créer un nouvel article</h1>
-//       <ArticleForm/>
-//     </div>
-//   );
-// }
+  return (
+    <div className="admin-new-article">
+      <h1>Créer un nouvel article</h1>
+      <ArticleForm/>
+    </div>
+  );
+}
