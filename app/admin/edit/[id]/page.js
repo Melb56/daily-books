444fd36e6637c36b7dcfa6e605d6de6678622bee
Melanie <melanie.bruzac@outlook.fr>
2025-06-05 09:22:05 +0200

    correction

--- app/admin/edit/[id]/page.js
+++ app/admin/edit/[id]/page.js
@@ -1,6 +1,6 @@
 import { prisma } from '@/lib/prisma'; 
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/lib/auth';
+import authOptions from '@/lib/auth';
 import { redirect } from 'next/navigation';
 import EditForm from '@/components/EditForm';
 
