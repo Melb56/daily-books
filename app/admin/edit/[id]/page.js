0e106f9c024c1e08bccdd62a253789ac77bc6bd1
Melanie <melanie.bruzac@outlook.fr>
2025-06-05 14:45:12 +0200

    t8

--- app/admin/edit/[id]/page.js
+++ app/admin/edit/[id]/page.js
@@ -1,36 +1,37 @@
-// import { prisma } from '@/lib/prisma'; 
-// import { getServerSession } from 'next-auth';
-// import { authOptions } from '@/lib/auth';
+import { prisma } from '@/lib/prisma'; 
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
 // import { auth } from '@/lib/auth';
-// import { redirect } from 'next/navigation';
-// import EditForm from '@/components/EditForm';
+import { redirect } from 'next/navigation';
+import EditForm from '@/components/EditForm';
 
 
 
-// export default async function EditArticlePage({ params }) {
-//   const articleId = parseInt(params.id, 10);
-//   const session = await getServerSession(authOptions);
-//   const resolvedParams = await params; 
-//   const { id } = resolvedParams; 
-//   const session = await auth()
+export default async function EditArticlePage({ params }) {
+  // const articleId = parseInt(params.id, 10);
+   const numericId = parseInt(params.id, 10);
+  const session = await getServerSession(authOptions);
+  // const resolvedParams = await params; 
+  // const { id } = resolvedParams; 
+  // const session = await auth()
 
-//   if (!session || session.user.role !== 'ADMIN') {
-//     redirect('/login');
-//   }
-//    const numericId = parseInt(id, 10);
+  if (!session || session.user.role !== 'ADMIN') {
+    redirect('/login');
+  }
+  //  const numericId = parseInt(id, 10);
 
-//   const article = await prisma.article.findUnique({
-//     where: { id: numericId },
-//   });
+  const article = await prisma.article.findUnique({
+    where: { id: numericId },
+  });
 
-//   if (!article) {
-//     return <div>Article introuvable.</div>;
-//   }
+  if (!article) {
+    return <div>Article introuvable.</div>;
+  }
 
-//   return (
-//     <div className="edit-article-page">
-//       <h1>Modifier l’article</h1>
-//       <EditForm article={article} />
-//     </div>
-//   );
-// }
+  return (
+    <div className="edit-article-page">
+      <h1>Modifier l’article</h1>
+      <EditForm article={article} />
+    </div>
+  );
+}
