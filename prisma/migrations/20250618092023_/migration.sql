d7fb3d0249d0cd827ebc0d74eea2a04aba1277b5
Melanie <melanie.bruzac@outlook.fr>
2025-06-19 15:10:41 +0200

    migration

--- prisma/migrations/20250618092023_/migration.sql
+++ prisma/migrations/20250618092023_/migration.sql
@@ -66,8 +66,8 @@ ALTER TABLE `Authenticator` ADD CONSTRAINT `Authenticator_userId_fkey` FOREIGN K
 
 -- RedefineIndex
 CREATE INDEX `Account_userId_idx` ON `Account`(`userId`);
-DROP INDEX `Account_userId_fkey` ON `account`;
+ALTER TABLE `account` DROP FOREIGN KEY `Account_userId_fkey`;
 
 -- RedefineIndex
 CREATE INDEX `Session_userId_idx` ON `Session`(`userId`);
-DROP INDEX `Session_userId_fkey` ON `session`;
+ALTER TABLE `session` DROP FOREIGN KEY `Session_userId_fkey`;
