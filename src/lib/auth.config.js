08ffff71a4e5616f4f438f4e20c36b4850a19048
Melanie <melanie.bruzac@outlook.fr>
2025-06-19 12:09:00 +0200

    modif aut.confi

--- src/lib/auth.config.js
+++ src/lib/auth.config.js
@@ -1,13 +1,29 @@
-import Google from 'next-auth/providers/google';
+// import Google from 'next-auth/providers/google';
+// import NextAuth from "next-auth";
 
+// const authConfig = {
+//     providers: [
+//         Google({
+//             clientId: process.env.GOOGLE_CLIENT_ID,
+//             clientSecret: process.env.GOOGLE_CLIENT_SECRET
+//         })
+//     ],
+//     secret: process.env.NEXTAUTH_SECRET,
+// };
 
-const authConfig = {
-    providers: [
-        Google({
-            clientId: process.env.AUTH_GOOGLE_ID,
-            clientSecret: process.env.AUTH_GOOGLE_SECRET
-        })
-    ]
-};
+// export default authConfig;
 
-export default authConfig;
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+});
+
+export default handler;
